95682511a9966dea8827fe75c17d2632e1e75253
idkNicks <yangdaeyeong0808@gmail.com>
2024-01-16 21:19:54 +0900

    add: Main instance field

--- src/main/java/xyz/starly/astralshop/AstralShop.java
+++ src/main/java/xyz/starly/astralshop/AstralShop.java
@@ -1,11 +1,16 @@
 package xyz.starly.astralshop;
 
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 import xyz.starly.astralshop.api.AstralShopPlugin;
 
 public class AstralShop extends JavaPlugin implements AstralShopPlugin {
 
+    @Getter
+    private static AstralShop instance;
+
     @Override
     public void onEnable() {
+        instance = this;
     }
 }
