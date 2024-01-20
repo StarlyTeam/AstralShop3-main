052bf784992d76951913847b42257c34aa73635d
YuInseo <inseo0121@gmail.com>
2024-01-20 19:48:15 +0900

    feat: add SQL syntax

--- src/main/java/xyz/starly/astralshop/registry/SQLShopRegistry.java
+++ src/main/java/xyz/starly/astralshop/registry/SQLShopRegistry.java
@@ -2,33 +2,53 @@ package xyz.starly.astralshop.registry;
 
 import xyz.starly.astralshop.api.registry.ShopRegistry;
 import xyz.starly.astralshop.api.shop.Shop;
+import xyz.starly.astralshop.database.SQLInjector;
 
+import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class SQLShopRegistry implements ShopRegistry {
+    private Logger LOGGER;
+
+    private SQLInjector sqlInjector;
+
+    public SQLShopRegistry(SQLInjector sqlInjector) {
+        this.sqlInjector = sqlInjector;
+    }
 
     @Override
-    public boolean createShop(String s, int i) {
+    public void loadShops() {
+
+    }
+
+    @Override
+    public void saveShops() {
+
+    }
+
+    @Override
+    public boolean createShop(String name, String guiTitle) {
+        String sql = "CREATE TABLE " + name + "_shop ( name VARCHAR(259) )";
+
+        try {
+            sqlInjector.createTable(sql);
+        } catch (SQLException e) {
+            LOGGER.info("mysql 에러 " + e);
+        }
+
         return false;
     }
 
     @Override
-    public boolean deleteShop(String s) {
+    public boolean deleteShop(String name) {
+        String sql = "";
+
         return false;
     }
 
     @Override
-    public boolean updateShop(String s) {
-        return false;
-    }
-
-    @Override
-    public boolean updateShops() {
-        return false;
-    }
-
-    @Override
-    public Shop getShop(String s) {
+    public Shop getShop(String name) {
         return null;
     }
 
