928e789025cd7ad4bdb078c3c6fbde5e0a1f8634
YuInseo <inseo0121@gmail.com>
2024-01-20 19:49:59 +0900

    feat: added SQLInjector register algorithm

--- src/main/java/xyz/starly/astralshop/AstralShop.java
+++ src/main/java/xyz/starly/astralshop/AstralShop.java
@@ -4,9 +4,13 @@ import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 import xyz.starly.astralshop.api.AstralShopPlugin;
 import xyz.starly.astralshop.api.registry.ShopRegistry;
+import xyz.starly.astralshop.command.TestShopCommand;
 import xyz.starly.astralshop.command.TestShopItemCommand;
+import xyz.starly.astralshop.database.SQLInjector;
 import xyz.starly.astralshop.registry.SQLShopRegistry;
 import xyz.starly.astralshop.registry.YamlShopRegistry;
+
+import java.sql.SQLException;
 
 public class AstralShop extends JavaPlugin implements AstralShopPlugin {
 
@@ -21,12 +25,28 @@ public class AstralShop extends JavaPlugin implements AstralShopPlugin {
         instance = this;
 
         if (getConfig().getBoolean("database.use")) {
-            shopRegistry = new SQLShopRegistry();
+            SQLInjector sqlInjector;
+
+            String url = getConfig().getString("url");
+            String user = getConfig().getString("user");
+            String password = getConfig().getString("password");
+            String dbName = getConfig().getString("dbName");
+
+            try {
+                sqlInjector = new SQLInjector(url, user, password, dbName);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            shopRegistry = new SQLShopRegistry(sqlInjector);
         } else {
             shopRegistry = new YamlShopRegistry(this);
         }
-        shopRegistry.loadShops();
 
+        shopRegistry.loadShops();
+        getCommand("shop").setExecutor(new TestShopCommand(shopRegistry));
         getCommand("shopitem").setExecutor(new TestShopItemCommand(shopRegistry));
     }
 }
