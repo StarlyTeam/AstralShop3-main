90e3a6c959a5d32e29d788744efd848a160ff2bb
YuInseo <inseo0121@gmail.com>
2024-01-21 14:36:33 +0900

    edit: ConnectionPoolManager initialized

--- src/main/java/xyz/starly/astralshop/AstralShop.java
+++ src/main/java/xyz/starly/astralshop/AstralShop.java
@@ -7,12 +7,9 @@ import xyz.starly.astralshop.api.AstralShopPlugin;
 import xyz.starly.astralshop.api.registry.ShopRegistry;
 import xyz.starly.astralshop.command.TestShopCommand;
 import xyz.starly.astralshop.command.TestShopItemCommand;
-import xyz.starly.astralshop.database.SQLInjector;
+import xyz.starly.astralshop.database.ConnectionPoolManager;
 import xyz.starly.astralshop.registry.SQLShopRegistry;
 import xyz.starly.astralshop.registry.YamlShopRegistry;
-
-import java.sql.SQLException;
-import java.util.logging.Level;
 
 public class AstralShop extends JavaPlugin implements AstralShopPlugin {
 
@@ -25,25 +22,15 @@ public class AstralShop extends JavaPlugin implements AstralShopPlugin {
     @Override
     public void onEnable() {
         instance = this;
+        saveDefaultConfig();
 
         if (getConfig().getBoolean("mysql.use")) {
-            SQLInjector sqlInjector = null;
+            ConnectionPoolManager.initializingPoolManager(getConfig());
+            ConnectionPoolManager pool = ConnectionPoolManager.getInternalPool();
 
-            String url = getConfig().getString("mysql.url");
-            String user = getConfig().getString("mysql.user");
-            String password = getConfig().getString("mysql.password");
-            String dbName = getConfig().getString("mysql.dbName");
+            getLogger().info("성공적으로 MYSQL 연결하였습니다.");
 
-            try {
-                sqlInjector = new SQLInjector(url, user, password, dbName);
-                getLogger().info("성공적으로 MYSQL 연결하였습니다.");
-            } catch (ClassNotFoundException e) {
-                getLogger().log(Level.WARNING, "JDBC Driver 클래스를 찾을 수 없습니다. " + e.getMessage());
-            } catch (SQLException e) {
-                getLogger().log(Level.WARNING, "MYSQL 연결 오류 " + e.getMessage());
-            }
-
-            shopRegistry = new SQLShopRegistry(sqlInjector);
+            shopRegistry = new SQLShopRegistry(pool);
         } else {
             shopRegistry = new YamlShopRegistry(this);
         }
@@ -51,7 +38,11 @@ public class AstralShop extends JavaPlugin implements AstralShopPlugin {
         shopRegistry.loadShops();
         getCommand("shop").setExecutor(new TestShopCommand(shopRegistry));
         getCommand("shopitem").setExecutor(new TestShopItemCommand(shopRegistry));
+    }
 
-        saveDefaultConfig();
+    @Override
+    public void onDisable() {
+        ConnectionPoolManager pool = ConnectionPoolManager.getInternalPool();
+        if (pool != null) pool.closePool();
     }
 }
