44b76443083a873605efc745b38eeeec5a250d3d
idkNicks <yangdaeyeong0808@gmail.com>
2024-01-23 19:52:04 +0900

    refactor: STMessage code

--- src/main/java/xyz/starly/astralshop/language/STMessage.java
+++ src/main/java/xyz/starly/astralshop/language/STMessage.java
@@ -14,17 +14,23 @@ public class STMessage {
     private String message;
 
     public void send(CommandSender sender) {
-        if (message.isEmpty()) return;
+        if (message.isEmpty()) {
+            return;
+        }
         sender.sendMessage(prefix + message);
     }
 
     public void send(ConsoleCommandSender console) {
-        if (message.isEmpty()) return;
+        if (message.isEmpty()) {
+            return;
+        }
         console.sendMessage(prefix + message);
     }
 
     public void broadcast() {
-        if (message.isEmpty()) return;
+        if (message.isEmpty()) {
+            return;
+        }
         AstralShop.getInstance().getServer().broadcastMessage(prefix + message);
     }
 
