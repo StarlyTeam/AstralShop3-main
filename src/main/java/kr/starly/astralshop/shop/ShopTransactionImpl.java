4c0c819afb694693a9768098374725abd37a1b73
호예준 <hyjcompany30@naver.com>
2024-06-22 19:10:38 +0900

    feat: make ShopTransaction ConfigurationSerializable

--- src/main/java/kr/starly/astralshop/shop/ShopTransactionImpl.java
+++ src/main/java/kr/starly/astralshop/shop/ShopTransactionImpl.java
@@ -8,8 +8,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @AllArgsConstructor
 @Getter
@@ -26,4 +29,19 @@ public class ShopTransactionImpl implements ShopTransaction {
 
     private final ShopItem item;
     private final int amount;
+
+    @NotNull
+    @Override
+    public Map<String, Object> serialize() {
+        Map<String, Object> result = new HashMap<>();
+        result.put("player", player.getUniqueId().toString());
+        result.put("type", type.name());
+        result.put("date", date.getTime());
+        result.put("shop", shop.getName());
+        result.put("page", page);
+        result.put("slot", slot);
+        result.put("amount", amount);
+
+        return result;
+    }
 }
